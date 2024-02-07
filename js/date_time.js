d6366532aa0287abbb01439954f3e11642c8defb
Drew Wheeler <wheelercs@protonmail.com>
2024-02-07 07:56:20 -0600

    Added Calendar Information
    Added Javascript to pull calendar information from user's browser and display it on the home page. Have some plans for this later down the line.

--- js/date_time.js
+++ js/date_time.js
@@ -1,11 +1,13 @@
 
-function update_clock() {
-    insert_clock()
-    setTimeout(update_clock, 16); // Update clock on webpage ~60 times per second
+function update_time_elements() {
+    insert_clock();
+    insert_weekday();
+    insert_calendar();
+    setTimeout(update_time_elements, 16);
 }
 
 function insert_clock() {
-    document.getElementById("local_clock").innerHTML = get_clock_string();
+    document.getElementById("local_clock").textContent = get_clock_string();
 }
 
 function get_clock_string() {
@@ -14,6 +16,11 @@ function get_clock_string() {
     time_hours = date_time.getHours();
     time_minutes = date_time.getMinutes();
     time_seconds = date_time.getSeconds();
+    // Convert military time to 12-hour time
+    clock_period = "AM"
+    if(time_hours > 12)
+        clock_period = "PM"
+    time_hours = time_hours % 12;
     // Add a zero in front of single-digit numbers
     if(time_hours < 10) {
         time_hours = "0" + time_hours;
@@ -25,6 +32,34 @@ function get_clock_string() {
         time_seconds = "0" + time_seconds;
     }
     // Concat time strings into one large clock
-    time_string = time_hours.toString() + ":" + time_minutes.toString() + ":" + time_seconds.toString();
+    time_string = time_hours.toString() + ":" + time_minutes.toString() + ":" + time_seconds.toString() + " " + clock_period;
     return time_string;
+}
+
+function insert_weekday() {
+    document.getElementById("local_weekday").textContent = get_weekday_string();
+}
+
+function get_weekday_string() {
+    // Get the weekday
+    date_time = new Date();
+    calendar_week_day = date_time.toLocaleString("default", {weekday: "long"});
+    return calendar_week_day;
+}
+
+function insert_calendar() {
+    document.getElementById("local_calendar").textContent = get_calendar_string();
+}
+
+function get_calendar_string() {
+    // Get calendar information from user's browser
+    date_time = new Date();
+    calendar_day = date_time.getDate();
+    calendar_year = date_time.getFullYear();
+
+    calendar_month = date_time.toLocaleString("default", {month: "long"});
+
+    calendar_string = calendar_month + " " + calendar_day.toString() + ", " + calendar_year.toString();
+
+    return calendar_string;
 }
