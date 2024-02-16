bd205f5e87db95754bbb370b487987f903c809c7
Drew Wheeler <wheelercs@protonmail.com>
2024-02-15 19:06:35 -0600

    Working on Binary Clock
    Working on implementing the binary clock portion of the clock on the home page.

--- js/date_time.js
+++ js/date_time.js
@@ -1,3 +1,8 @@
+// Unicode circles for binary clock:
+// Empty Circle: &#x25CB;
+// Filled Circle: &#x25CF;
+
+// === Main Loop =======================================================================================================
 
 function update_time_elements() {
     insert_clock();
@@ -6,8 +11,12 @@ function update_time_elements() {
     setTimeout(update_time_elements, 16);
 }
 
+
+// === Local Time ======================================================================================================
+
 function insert_clock() {
     document.getElementById("local_clock").textContent = get_clock_string();
+    document.getElementById("binary_local_clock").textContent = get_binary_clock_string();
 }
 
 function get_clock_string() {
@@ -36,6 +45,53 @@ function get_clock_string() {
     return time_string;
 }
 
+function get_binary_clock_string() {
+    // Get the time from the system
+    date_time = new Date();
+    time_hours = date_time.getHours();
+    time_minutes = date_time.getMinutes();
+    time_seconds = date_time.getSeconds();
+    // Determine if AM or PM and convert from military time
+    is_pm = "0";
+    if(time_hours > 12) {
+        is_pm = "1";
+        time_hours %= 12;
+    }
+    // Convert time from decimal to binary string
+    bin_time_hours = time_hours.toString(2);
+    bin_time_minutes = time_minutes.toString(2);
+    bin_time_seconds = time_minutes.toString(2);
+    // Pad binary time strings if needed
+    // Hours
+    string_difference = 3 - bin_time_hours.length;
+    for(let i = 0; i < string_difference; i++)
+        bin_time_hours = "0" + bin_time_hours;
+    // Minutes
+    string_difference = 6 - bin_time_minutes.length;
+    for(let i = 0; i < string_difference; i++)
+        bin_time_minutes = "0" + bin_time_minutes;
+    // Seconds
+    string_difference = 6 - bin_time_seconds.length;
+    for(let i = 0; i < string_difference; i++)
+        bin_time_seconds = "0" + bin_time_seconds;
+    // Concat binary strings to one large string
+    binary_time = is_pm + bin_time_hours + bin_time_minutes + bin_time_seconds;
+    // Convert 0 and 1 to Unicode circles
+    let empty_circle = '\u{25CB}';
+    let filled_circle = '\u{25CF}';
+    for(let i = 0; i < binary_time.length; i++) {
+        if(binary_time[i] == '0')
+            binary_time[i] = empty_circle;
+        else
+            binary_time[i] = filled_circle;
+    }
+
+    return binary_time;
+}
+
+
+// === Weekday Info ====================================================================================================
+
 function insert_weekday() {
     document.getElementById("local_weekday").textContent = get_weekday_string();
 }
@@ -46,6 +102,9 @@ function get_weekday_string() {
     calendar_week_day = date_time.toLocaleString("default", {weekday: "long"});
     return calendar_week_day;
 }
+
+
+// === Calendar Info ===================================================================================================
 
 function insert_calendar() {
     document.getElementById("local_calendar").textContent = get_calendar_string();
